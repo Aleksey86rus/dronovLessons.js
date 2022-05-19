6207505576fc4dde55d26da141338a7a6ff77179
Aleksey86rus <Coppola80@mail.ru>
2022-05-19 21:46:19 +0500

    added dronovLessons.js

---
+++ 1.12-homework.js
@@ -0,0 +1,21 @@
+// let num = prompt('Write your number:');
+// console.log(num * 2.54);
+
+// let a = 123456789;
+// console.log(a % 3);
+
+// let sum2 = 500;
+// console.log(sum2);
+// sum2 += 100;
+// console.log(sum2);
+// sum2 += 100;
+// console.log(sum2);
+// console.log(sum2);
+
+let sum2 = 500;
+console.log(sum2);
+sum2 += 100;
+console.log(sum2);
+sum2 += 100;
+console.log(sum2);
+console.log(sum2);
