5e86fc9084254f0c2470c1c22b84a9d02497a2ca
Aleksey86rus <Coppola80@mail.ru>
2022-05-19 22:56:51 +0500

    add homewor

--- 1.12-homework.js
+++ 1.12-homework.js
@@ -4,10 +4,17 @@
 // let a = 123456789;
 // console.log(a % 3);
 
-let sum2 = 500;
-console.log(sum2);
-sum2 += 100;
-console.log(sum2);
-sum2 += 100;
-console.log(sum2);
-console.log(sum2);
+// let sum2 = 500;
+// const average = (sum2 + 600 + 700 / 3);
+// // sum2 += 100;
+// // console.log(sum2);
+// // sum2 += 100;
+// // console.log(sum2);
+
+// let count = 3;
+// console.log(average);
+
+// let a = Math.sqrt(7);
+// console.log(a);
+
+
